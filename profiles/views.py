887961fb7b6cf9301641be873a51d362af5a0895
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-18 17:00:14 +0000

    Fix field not valid for is_owner in serializers.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -16,7 +17,9 @@ class ProfileList(APIView):
     """
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(
+            profiles, many=True, context={'request': request}
+        )
         return Response(serializer.data)
 
 
