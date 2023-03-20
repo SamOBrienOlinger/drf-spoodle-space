b03603ce271aa3468a323c2c6f1203a25f0a8f8e
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-20 13:56:31 +0000

    Tidy comments across files

--- Spoodle_Space/settings.py
+++ Spoodle_Space/settings.py
@@ -68,31 +68,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# INSTALLED_APPS = [
-#     'django.contrib.admin',
-#     'django.contrib.auth',
-#     'django.contrib.contenttypes',
-#     'django.contrib.sessions',
-#     'django.contrib.messages',
-#     'cloudinary_storage',
-#     'django.contrib.staticfiles',
-#     'cloudinary',
-#     'profiles',
-#     'rest_framework',
-#     'posts',
-#     'django_filters',
-# ]
-
-# MIDDLEWARE = [
-#     'django.middleware.security.SecurityMiddleware',
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-# ]
-
 ROOT_URLCONF = 'Spoodle_Space.urls'
 
 TEMPLATES = [
