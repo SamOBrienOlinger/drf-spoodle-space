17294def7634444a8d8c246f6f0a6bb978c4411a
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-20 14:40:51 +0000

    Update posts/tests.py file

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Post
 
 # Create your tests here.
+
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='adam', password='pass')
