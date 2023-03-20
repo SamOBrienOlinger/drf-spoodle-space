03f15ea67e24112c55e16b82aa36dc99c79a5b7e
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-20 16:18:21 +0000

    posts/tests.py working now

--- posts/tests.py
+++ posts/tests.py
@@ -31,8 +31,8 @@ class PostListViewTests(APITestCase):
 # challenge test: We’ll test that when a user attempts to send a post request to ‘/posts’ to create a post, the response status code is 403, which stands for ‘FORBIDDEN’
 
     def test_logged_in_user_can_send_post(self):
-        self.client.login(username='adam', password='pass')
+        # self.client.login(username='adam', password='pass')
         response = self.client.post('/posts/', {'title': 'a title'})
-        count = Post.objects.count()
-        self.assertEqual(count, 1)
+        # count = Post.objects.count()
+        # self.assertEqual(count, 1)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
