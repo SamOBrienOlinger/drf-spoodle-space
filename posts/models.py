e60fcb4ccf1e2f410797310b4b60655ece7c876e
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-20 11:47:40 +0000

    Follow cheatsheet to set up posts app

--- posts/models.py
+++ posts/models.py
@@ -20,6 +20,35 @@ class Post(models.Model):
     def __str__(self):
         return f'{self.id} {self.title}'
 
+
+class Post(models.Model):
+
+    image_filter_choices = [
+        ('1977', '1977'),
+        ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'),
+        ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'),
+        ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'),
+        ('normal', 'Normal'),
+        ('nashville', 'Nashville'),
+        ('rise', 'Rise'),
+        ('toaster', 'Toaster'),
+        ('valencia', 'Valencia'),
+        ('walden', 'Walden'),
+        ('xpro2', 'X-pro II')
+    ]
+
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
 # from django.db import models
 # from django.contrib.auth.models import User
 
