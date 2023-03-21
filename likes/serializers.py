c78b424298864c054b15a107860b4289d3c8d8de
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-21 17:28:08 +0000

    Add all files and code to likes app

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
 from .models import Like
+from django.db import IntegrityError
 
 
 class LikeSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerializerMethodField()
+    # is_owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Like()
         fields = [
             'id', 'owner', 'created_at', 'post'
         ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({'detail': 'possible cocka-duplicate'})
