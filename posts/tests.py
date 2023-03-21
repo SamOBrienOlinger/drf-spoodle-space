9adced7fca54ff72a8e4a7b525b361ed96696b42
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-21 15:22:40 +0000

    Test fail -posts/views.py", line 48, in get_object     raise Http404 NameError: name 'Http404' is not defined

--- posts/tests.py
+++ posts/tests.py
@@ -1,25 +1,20 @@
 from django.contrib.auth.models import User
+from .models import Post
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import Post
-
-# Create your tests here.
 
 
 class PostListViewTests(APITestCase):
     def setUp(self):
         User.objects.create_user(username='adam', password='pass')
 
-# test 1
-
     def test_can_list_posts(self):
         adam = User.objects.get(username='adam')
         Post.objects.create(owner=adam, title='a title')
         response = self.client.get('/posts/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print(response.data, len(response.data))
-
-# test 2
+        print(response.data)
+        print(len(response.data))
 
     def test_logged_in_user_can_create_post(self):
         self.client.login(username='adam', password='pass')
@@ -27,8 +22,6 @@ class PostListViewTests(APITestCase):
         count = Post.objects.count()
         self.assertEqual(count, 1)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-# challenge test: We’ll test that when a user attempts to send a post request to ‘/posts’ to create a post, the response status code is 403
 
     def test_user_not_logged_in_cant_create_post(self):
         response = self.client.post('/posts/', {'title': 'a title'})
@@ -66,3 +59,72 @@ class PostDetailViewTests(APITestCase):
         self.client.login(username='adam', password='pass')
         response = self.client.put('/posts/2/', {'title': 'a new title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+# from django.contrib.auth.models import User
+# from rest_framework import status
+# from rest_framework.test import APITestCase
+# from .models import Post
+
+# # Create your tests here.
+
+
+# class PostListViewTests(APITestCase):
+#     def setUp(self):
+#         User.objects.create_user(username='adam', password='pass')
+
+# # test 1
+
+#     def test_can_list_posts(self):
+#         adam = User.objects.get(username='adam')
+#         Post.objects.create(owner=adam, title='a title')
+#         response = self.client.get('/posts/')
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#         print(response.data, len(response.data))
+
+# # test 2
+
+#     def test_logged_in_user_can_create_post(self):
+#         self.client.login(username='adam', password='pass')
+#         response = self.client.post('/posts/', {'title': 'a title'})
+#         count = Post.objects.count()
+#         self.assertEqual(count, 1)
+#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+# # challenge test: We’ll test that when a user attempts to send a post request to ‘/posts’ to create a post, the response status code is 403
+
+#     def test_user_not_logged_in_cant_create_post(self):
+#         response = self.client.post('/posts/', {'title': 'a title'})
+#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+# class PostDetailViewTests(APITestCase):
+#     def setUp(self):
+#         adam = User.objects.create_user(username='adam', password='pass')
+#         brian = User.objects.create_user(username='brian', password='pass')
+#         Post.objects.create(
+#             owner=adam, title='a title', content='adams content'
+#         )
+#         Post.objects.create(
+#             owner=brian, title='another title', content='brians content'
+#         )
+
+#     def test_can_retrieve_post_using_valid_id(self):
+#         response = self.client.get('/posts/1/')
+#         self.assertEqual(response.data['title'], 'a title')
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+#     def test_cant_retrieve_post_using_invalid_id(self):
+#         response = self.client.get('/posts/999/')
+#         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+#     def test_user_can_update_own_post(self):
+#         self.client.login(username='adam', password='pass')
+#         response = self.client.put('/posts/1/', {'title': 'a new title'})
+#         post = Post.objects.filter(pk=1).first()
+#         self.assertEqual(post.title, 'a new title')
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+#     def test_user_cant_update_another_users_post(self):
+#         self.client.login(username='adam', password='pass')
+#         response = self.client.put('/posts/2/', {'title': 'a new title'})
+#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
