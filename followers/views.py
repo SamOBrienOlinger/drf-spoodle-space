da1296c8ff27e668e8ea356055076d32d4715dbd
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-23 13:05:37 +0000

    Fix typos in followers/serializers and views files

--- followers/views.py
+++ followers/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
 from Spoodle_Space.permissions import IsOwnerOrReadOnly
 from .models import Follower
-from .serializers import FollowSerializer
+from .serializers import FollowerSerializer
 
 # Create your views here.
 
@@ -10,16 +10,16 @@ class FollowerList(generics.ListCreateAPIView):
     """ List all followers. Create a follow if authenticated. The perform_create method associates the follow with the logged in user. """
 
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    serializer_class = FollowSerializer
-    queryset = Follow.objects.all()
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
-class FollowDetail(generics.RetrieveDestroyAPIView):
+class FollowerDetail(generics.RetrieveDestroyAPIView):
     """ Retrieve a like. No Update view, as users can only like or unlike a post. Destroy a like, i.e. unlike a post if owner of that like """
 
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Follower().objects.all()
-    serializer_class = FollowSerializer
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
