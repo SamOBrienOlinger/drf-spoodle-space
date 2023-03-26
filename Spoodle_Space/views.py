07b2cc000658cb7f86d4f6d11e0ec5017bec0578
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-26 13:26:51 +0000

    Add dj-rest-auth logout view fix

--- Spoodle_Space/views.py
+++ Spoodle_Space/views.py
@@ -5,3 +5,28 @@ from rest_framework.response import Response
 @api_view()
 def root_route(request):
     return Response({"Welcome to Spoodle Space! Our community of Cockapoopers is all about sharing ways of enjoying long ludicrous lives with the lovliest furry fluffy fellas on Earth"})
+
+
+# dj-rest-auth logout view fix
+@api_view(['POST'])
+def logout_route(request):
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
