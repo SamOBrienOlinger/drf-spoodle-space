38f68add7db11bd2ce10a4378365d782ac4f79a1
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-04 16:24:32 +0000

    Add and comment out DogProfile custom model

--- profiles/models.py
+++ profiles/models.py
@@ -13,6 +13,19 @@ class Profile(models.Model):
         upload_to='images/', default='../default_profile_qdjgyp'
     )
 
+# class DogProfile(models.mMdel):
+
+#         owner = models.OneToOneField(User, on_delete=models.CASCADE)
+#         created_at = models.DateTimeField(auto_now_add=True)
+#         updated_at = models.DateTimeField(auto_now=True)
+#         DogName	= models.CharField(max_length=255, blank=True)
+#         DogAge = IntegerField(default=0)
+#         DogColor = charField
+#         DogBio	= models.TextField(blank=True)
+#         DogImage = Imodels.ImageField(
+#         upload_to='images/', default='../default_dog-profile_gtehul.png'
+#     )
+
     class Meta:
         ordering = ['-created_at']
 
