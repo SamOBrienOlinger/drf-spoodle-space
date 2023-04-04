b514ca6793ecc64ca7bbf577007d7dc4a923b4ee
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-04 16:49:58 +0000

    Add and comment out DogProfile

--- profiles/apps.py
+++ profiles/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
+
+
+# class DogProfile(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'profiles'
