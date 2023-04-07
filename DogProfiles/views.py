483118861595880b183428416d124faf9d3475c0
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-07 12:24:04 +0000

    Set up DogProfiles/serializers.py

--- DogProfiles/views.py
+++ DogProfiles/views.py
@@ -1,46 +1,51 @@
 from django.shortcuts import render
-
-from django.db.models import Count
+# from django.db.models import Count
 from rest_framework import generics, filters
 from Spoodle_Space.permissions import IsOwnerOrReadOnly
 from .models import DogProfile
-from .serializers import ProfileSerializer
+from .serializers import DogProfileSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
 
 class DogProfileList(generics.ListAPIView):
-    queryset = DogProfile.objects.annotate(
-        posts_count=Count('owner__post', distinct=True),
-        followers_count=Count('owner__followed', distinct=True),
-        following_count=Count('owner__following', distinct=True)
-    ).order_by('-created_at')
+    
+    # queryset = DogProfile.objects.annotate(
+    #     posts_count=Count('owner__post', distinct=True),
+    #     followers_count=Count('owner__followed', distinct=True),
+    #     following_count=Count('owner__following', distinct=True)
+    # ).order_by('-created_at')
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = DogProfileSerializer
+    queryset = DogProfile.objects.all()
+    
     filter_backends = [
         filters.OrderingFilter,
         DjangoFilterBackend,
     ]
+    
     filterset_fields = [
-        'owner__following__followed__profile',
-        'owner__followed__owner__profile',
-        # 'likes__owner__profile',
-        # 'owner__profile'
+        # 'owner__following__followed__profile',
+        # 'owner__followed__owner__profile',
+        # # 'likes__owner__profile',
+        # # 'owner__profile'
     ]
     ordering_fields = [
-        'posts_count',
-        'followers_count',
-        'following_count',
         'owner__following__created_at',
         'owner__followed__created_at',
     ]
 
 
-class ProfileDetail(generics.RetrieveUpdateAPIView):
+class DogProfileDetail(generics.RetrieveUpdateAPIView):
+
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = DogProfile.objects.annotate(
-        posts_count=Count('owner__post', distinct=True),
-        followers_count=Count('owner__followed', distinct=True),
-        following_count=Count('owner__following', distinct=True)
-    ).order_by('-created_at')
+    queryset = DogProfile.objects.all()
     serializer_class = DogProfileSerializer
+
+    # queryset = DogProfile.objects.annotate(
+    #     posts_count=Count('owner__post', distinct=True),
+    #     followers_count=Count('owner__followed', distinct=True),
+    #     following_count=Count('owner__following', distinct=True)
+    # ).order_by('-created_at')
+    # serializer_class = DogProfileSerializer
