9ae0d806c06db7b977d9224bb66ed90bad414cef
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-11 14:28:58 +0000

    Update SECRET_KEY syntax

--- Spoodle_Space/settings.py
+++ Spoodle_Space/settings.py
@@ -59,11 +59,11 @@ REST_AUTH_SERIALIZERS = {
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = {
-    'SECRET_KEY': os.environ.get('SECRET_KEY')
-}
+# SECRET_KEY = {
+#     'SECRET_KEY': os.environ.get('SECRET_KEY')
+# }
 
-# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
