2bfcaa008ca0fdb4e495a8b5fec32f4255a68349
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-11 11:00:45 +0000

    Change DEBUG to True to find 500 error

--- Spoodle_Space/settings.py
+++ Spoodle_Space/settings.py
@@ -66,7 +66,9 @@ SECRET_KEY = {
 # SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+# DEBUG = 'DEV' in os.environ
+
+DEBUG = True
 
 ALLOWED_HOSTS = [
    os.environ.get('ALLOWED_HOST'),
