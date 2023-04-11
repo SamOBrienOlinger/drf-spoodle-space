99bbf92f034180d156888f87f568436c12c3cd86
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-11 10:28:45 +0000

    Add DogProfile to profiles/serializers.py

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
-# from .models import DogProfile
+from dogprofiles.models import DogProfile
 from followers.models import Follower
 
 
