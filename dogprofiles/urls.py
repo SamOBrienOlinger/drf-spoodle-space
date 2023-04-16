cacf14ad5202f8199641dbf8a3d7efac5df78e55
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-16 14:44:12 +0000

    Add missing comma

--- dogprofiles/urls.py
+++ dogprofiles/urls.py
@@ -3,5 +3,5 @@ from dogprofiles import views
 
 urlpatterns = [
     path('dogprofiles/', views.DogProfileList.as_view()),
-    path('dogprofiles/<int:pk>/', views.DogProfileDetail.as_view())
+    path('dogprofiles/<int:pk>/', views.DogProfileDetail.as_view()),
 ]
