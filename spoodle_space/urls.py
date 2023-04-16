2741fbb5593a6fd602e2642b89897d656a175d8e
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-16 20:34:37 +0000

    Update to match walkthrough

--- spoodle_space/urls.py
+++ spoodle_space/urls.py
@@ -35,18 +35,3 @@ urlpatterns = [
 
     path('', include('dogprofiles.urls')),
 ]
-
-# from .views import root_route
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api-auth/', include('rest_framework.urls')),
-#     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-#     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-#     path('', include('profiles.urls')),
-#     path('', include('posts.urls')),
-#     path('', include('comments.urls')),
-#     path('', include('likes.urls')),
-#     path('', include('followers.urls')),
-#     path('', root_route),
-# ]
