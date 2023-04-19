51790faeae7ce1a22ec2fc3b3e32031f1bc100bf
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-19 08:46:07 +0000

    Add fields and registerDogHealth model

--- doghealth/admin.py
+++ doghealth/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import DogHealth
 
-# Register your models here.
+admin.site.register(DogHealth)
--- doghealth/models.py
+++ doghealth/models.py
@@ -7,6 +7,13 @@ class DogHealth(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    vet_name = models.CharField(max_length=255, blank=True)
+    vet_phone = models.CharField(max_length=255, blank=True)
+    vet_email = models.CharField(max_length=255, blank=True)
+    chipped = models.CharField(max_length=255, blank=True)
+    kennel_cough = models.CharField(max_length=255, blank=True)
+    rabies = models.CharField(max_length=255, blank=True)
+    allergies = models.CharField(max_length=255, blank=True)
 
     class Meta:
         ordering = ['-created_at']
