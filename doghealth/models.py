0f105b85b205d0ee9cdefbe8827936ccf50e433a
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-19 06:50:02 +0000

    Add basic structure to doghealth/models.py

--- doghealth/models.py
+++ doghealth/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class DogHealth(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s Dog's health status"
+
+
+def create_doghealth(sender, instance, created, **kwargs):
+    if created:
+        DogHealth.objects.create(owner=instance)
+
+
+post_save.connect(create_doghealth, sender=User)
