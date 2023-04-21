80dcf16682e5899a33f599190fd03ebfe3a76f81
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-21 17:41:22 +0000

    Comment out dogprofiles import

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
-from dogprofiles.models import DogProfile
+# from dogprofiles.models import DogProfile
 from followers.models import Follower
 
 
