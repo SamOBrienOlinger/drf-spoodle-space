0fdce3bbd7f704d3b26ac03fae45fdf223806755
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-24 10:34:43 +0000

    Comment out from django.shortcuts import render

--- dogprofiles/views.py
+++ dogprofiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from rest_framework import generics, filters, permissions
 from spoodle_space.permissions import IsOwnerOrReadOnly
 from .models import DogProfile
