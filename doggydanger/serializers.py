1e7ccb85c495564fb428db526d623c241c822295
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-27 20:20:20 +0000

    ['CLIENT_ORIGIN_DEV'] changed again, is now 'eu96'

--- doggydanger/serializers.py
+++ doggydanger/serializers.py
@@ -7,20 +7,20 @@ class DoggyDangerSerializer(serializers.ModelSerializer):
     class meta:
         model = DoggyDanger
         fields = [
-                'owner',
-                'id',
-                'created_at',
-                'updated_at',
+            'owner',
+            'id',
+            'created_at',
+            'updated_at',
 
-                'bites_babies',
-                'bites_kids',
-                'bites_teenagers',
-                'bites_burglars',
-                'bites_bolsonaro',
-                'bites_trump',
-                'bites_thatcher',
-                'bites_reagan',
-                'bites_bush',
-                'bites_wbush',
-                'dangerously_cute',
-            ]
+            'bites_babies',
+            'bites_kids',
+            'bites_teenagers',
+            'bites_burglars',
+            'bites_bolsonaro',
+            'bites_trump',
+            'bites_thatcher',
+            'bites_reagan',
+            'bites_bush',
+            'bites_wbush',
+            'dangerously_cute',
+        ]
