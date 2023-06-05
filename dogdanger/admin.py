37d4164d1ff9ef61e53750e27c01248ed9a5b57d
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-06-05 15:40:23 +0000

    Remove fields from model

--- dogdanger/admin.py
+++ dogdanger/admin.py
@@ -5,5 +5,5 @@ from .models import DogDanger
 # admin.site.register(DogDanger)
 @admin.register(DogDanger)
 class DogDanger(admin.ModelAdmin):
-    list_filter = ('owner', 'bites_babies', 'bites_kids', 'bites_teenagers', 'bites_burglars', 'bites_bolsonaro', 'bites_trump', 'bites_thatcher', 'bites_reagan', 'bites_bush', 'bites_wbush', 'dangerously_cute')
-    list_display = ('owner', 'bites_babies', 'bites_kids', 'bites_teenagers', 'bites_burglars', 'bites_bolsonaro', 'bites_trump', 'bites_thatcher', 'bites_reagan', 'bites_bush', 'bites_wbush', 'dangerously_cute')
+    list_filter = ('owner', 'bites_babies', 'bites_kids', 'bites_teenagers', 'bites_burglars', 'dangerously_cute')
+    list_display = ('owner', 'bites_babies', 'bites_kids', 'bites_teenagers', 'bites_burglars', 'dangerously_cute')
