1c94f89a09fcec2905f1f0f5577d28839773d87c
SamOBrienOlinger <samtobo@yahoo.co.uk>
2025-06-26 13:39:17 +0000

    Switch back to CompressedStaticFilesStorage for Heroku deploy

--- spoodle_space/settings.py
+++ spoodle_space/settings.py
@@ -210,7 +210,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
